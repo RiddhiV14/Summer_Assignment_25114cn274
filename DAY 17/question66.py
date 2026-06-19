8348302d17d4fe9f2cb00047923c3f52cf0ebd30
RiddhiV14 <i.riddhiiii@gmail.com>
2026-06-19 22:20:30 +0530

    Update question66.py

--- DAY 17/question66.py
+++ DAY 17/question66.py
@@ -10,12 +10,6 @@ arr3 = []
 for i in range(n2):
     arr2.append(int(input("Enter element: ")))
 
-for i in arr1 : 
-    for j in arr2:
-        if i != j :
-            arr3.append(i)
-
-
 for i in arr1:
     if i not in arr3:
         arr3.append(i)
